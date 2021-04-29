ecc48ebd89b08d30cefaee12e6ab8e7501e38956
Jamppa1234 <finjarnom@gmail.com>
2021-04-29 11:15:20 +0300

    asennettu cors ja tehty api.js muutokset

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
 import "./App.css";
 
 function App() {
+    axios
+    .get("http://localhost:4000/hello")
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   return <div className="App">Hello World!</div>;
 }
 
